b69a198a1a4daa4af2be5b44f77a2b9807421cee
Ruben <76583185+mukherjeeruben@users.noreply.github.com>
2024-02-29 13:00:32 +0000

    changed timeline description
    changed timeline description

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -38,12 +38,12 @@ export const projects = [
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Tata Consultancy Services, Digital', },
+  { year: 2017, text: 'Tata Consultancy Services (Digital), IT Analyst, Mumbai (India)',},
   { year: 2018, text: 'Worked as a cross platform application developer - Python ,Typescript and C#', },
   { year: 2019, text: 'Started working as a freelance consulatnt on Dash, Flask and Django Microservices', },
   { year: 2020, text: 'Created custom apps for Aerospace domain in Data Analyics with statistical models and visualization frameworks', },
-  { year: 2021, text: 'Dublin City University - Masters in Artificial Intelligence', },
-  { year: 2022, text: 'Danalto, Dublin (IOT Startup) - Senior Software Engineer', },
-  { year: 2023, text: 'Retail inMotion (Lufthansa Service Holding AG) - Data Engineer/Scientist', },
+  { year: 2021, text: 'Dublin City University - Masters in Artificial Intelligence, Dublin (Ireland)', },
+  { year: 2022, text: 'Danalto (IOT Startup) - Senior Software Engineer, Dublin (Ireland)', },
+  { year: 2023, text: 'Retail inMotion (Lufthansa Service Holding AG) - Data Engineer/Scientist, Dublin (Ireland)', },
   { year: 2024, text: 'Consulting and Building Analytics Products', },
 ];
