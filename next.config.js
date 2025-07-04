5215a14446288f8e952992be4f32a01f6f831824
Ruben <76583185+mukherjeeruben@users.noreply.github.com>
2025-07-04 08:15:59 +0100

    update

--- next.config.js
+++ next.config.js
@@ -2,15 +2,6 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
- 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
 }
  
 module.exports = nextConfig
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,27 +7,7 @@ import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyle
 
 const Header = () =>  (
   <Container>
-    <Div1>
-      <Link href="/">
-        {/* <a style={{ display: 'flex', alignItems: 'center', color:"white" }}> */}
-          {/* <DiCode size="3rem" />  */}
-          {/* <span>
-          <table>
-            <tr>
-              <td>◉ Data Engineer</td>
-            </tr>
-            <tr>
-              <td>◉ Data Scientist</td>
-            </tr>
-            <tr>
-              <td>◉ Software Engineer</td>
-            </tr>
-          </table>  
-          </span> */}
-          
-        {/* </a> */}
-      </Link>
-    </Div1>
+    
     {/* <Div2>
       <li>
         <Link href="#projects">
@@ -46,19 +26,7 @@ const Header = () =>  (
       </li>        
     </Div2> */}
       <Div3>
-      <table>
-            <tr>
-              <td><SocialIcons href="https://github.com/mukherjeeruben">
-                  <AiFillGithub size="2.6rem" />
-                </SocialIcons>
-              </td>
-              <td>
-              <SocialIcons href="https://www.linkedin.com/in/rubenmukherjee/">
-                <AiFillLinkedin size="2.6rem" />
-              </SocialIcons>
-              </td>
-            </tr>
-          </table> 
+      
         
        
         {/* <SocialIcons href="https://google.com">
